93b9386c33a6a3d59e4596cc393eb5f67b06c1fe
Slavik <slavik1058@gmail.com>
2026-06-09 21:49:34 +0200

    add SearchBox component with debounced input; include styles and dependencies

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react"
+import{useDebouncedCallback} from "use-debounce"
+import css from "./SearchBox.module.css"
+
 export default function SearchBox(){
+    const [inputValue, setInputValue] = useState("");
+
+    const handleChange = useDebouncedCallback((e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value), 300);
+
     return(
-        <>
-        
-        </>
+        <div className="css.input">
+            <input type="text" defaultValue={inputValue} onChange={handleChange}/>
+            <p>{inputValue}</p>
+        </div>
     )
 }
